320ad060d7519014282054f226ee51cd1c377977
Andre Cortes <52327276+andrecortes1997@users.noreply.github.com>
2024-05-06 05:10:44 +0000

    fetch

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,8 +2,30 @@ import React, { useState } from "react";
 
 
 const Home = () => {
+	
 	const [inputValue, setInputValue] = useState("");
 	const [toDos, setToDos] = useState([]);
+	fetch('https://redesigned-space-xylophone-wgwr6w455pjh5wg9-3000.app.github.dev/', {
+		method: "PUT",
+		body: JSON.stringify(toDos),
+		headers: {
+		  "Content-Type": "application/json"
+		}
+	  })
+	  .then(resp => {
+		  console.log(resp.ok); // Will be true if the response is successful
+		  console.log(resp.status); // The status code=200 or code=400 etc.
+		  console.log(resp.text()); // Will try to return the exact result as a string
+		  return resp.json(); // (returns promise) Will try to parse the result as JSON and return a promise that you can .then for results
+	  })
+	  .then(data => {
+		  // Here is where your code should start after the fetch finishes
+		  console.log(data); // This will print on the console the exact object received from the server
+	  })
+	  .catch(error => {
+		  // Error handling
+		  console.error(error);
+	  });
 	return (
 		<div className="container">
 			<h1>My Task List</h1>
@@ -32,6 +54,8 @@ const Home = () => {
 
 			</ul>
 			<div>{toDos.length} task</div>
+
+
 		</div>
 	);
 };
